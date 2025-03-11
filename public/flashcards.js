cceae02f34051bd7cd048d98f0d375bfd2b91095
Jon Bennett <jdbennet2001@yahoo.com>
2025-03-11 10:40:54 -0400

    Update JSON word list

--- public/flashcards.js
+++ public/flashcards.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $.getJSON('flashcards.json', function(data) {
         const wordMap = data.flashcards;
         const words = Object.keys(wordMap);
-        
+        let currentWord = '';
+
         function getRandomChoices(correctWord) {
             let choices = [wordMap[correctWord]];
             let shuffledWords = _.shuffle(words);
@@ -17,11 +18,11 @@ $(document).ready(function() {
         }
         
         function generateFlashcard() {
-            const randomWord = words[Math.floor(Math.random() * words.length)];
-            const choices = getRandomChoices(randomWord);
+            currentWord = words[Math.floor(Math.random() * words.length)];
+            const choices = getRandomChoices(currentWord);
             
             let htmlContent = `<div class='flashcard'>
-                <h2>${randomWord}</h2>
+                <h2>${currentWord}</h2>
                 <ul class='choices'>`;
             
             choices.forEach(choice => {
@@ -31,6 +32,20 @@ $(document).ready(function() {
             htmlContent += `</ul></div>`;
             $('body').html(htmlContent);
         }
+        
+        $(document).on('click', '.choice', function() {
+            const selectedChoice = $(this).text();
+            if (selectedChoice === wordMap[currentWord]) {
+                $(this).css('background', 'green');
+                setTimeout(generateFlashcard, 1000);
+            } else {
+                $('.choices').html(`<li class='correct-answer'>${wordMap[currentWord]}</li>`);
+            }
+        });
+        
+        $(document).on('click', '.correct-answer', function() {
+            generateFlashcard();
+        });
         
         generateFlashcard();
     }).fail(function() {
